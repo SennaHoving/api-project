39c5ecb5582fb7af7769f11a0fb6a0042e8887c8
Senna Hoving <senna.hoving@gmail.com>
2026-05-06 20:50:30 +0200

    start create workout page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,25 @@
-console.log("sup")
+const dragItems = document.querySelectorAll("li"); 
+const dragFields = document.querySelectorAll(".field");
+
+let draggedElement = null;
+
+dragItems.forEach(item => {
+    item.setAttribute("draggable", "true");
+
+    item.addEventListener("dragstart", (event) => {
+        draggedElement = item;
+    });
+})
+
+dragFields.forEach(field => {
+    field.addEventListener("dragover", (e) => {
+        e.preventDefault(); 
+    })
+
+    field.addEventListener("drop", (e) => {
+        e.preventDefault();
+        if (draggedElement) {
+            field.appendChild(draggedElement);
+        }
+    })
+})
